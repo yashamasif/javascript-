acc7c0579e0b9370745c6a8593fc365bc5494915
yashamasif <yashamasif58@gmail.com>
2024-12-29 11:39:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+alert("Welcome to Javascript");
+prompt("What is your name");
+var firstname =prompt("Enter Your first Name");
+var secondname =prompt("Enter Your Second Name");
+var fullname = (firstname + secondname);
+alert("full name =");
+var age = prompt("enter your age");
+var yearofbirth=prompt("enter your yearofbirth");
+var currentyear =prompt("enter current year");
+var yourage =(currentyear-yearofbirth);
+alert("sum of ages =");
+var age=prompt("enter your age");
+var friendage=prompt("enter your friend's age");
+var sumofages=(age-friendage);
+alert("sum of ages =");
+var difference=(firstnum-secondnum);
+alert("difference");
+var product=(firstnum*secondnum);
+alert("product =");
+var firstvalue=4
+var secondvalue=10
+alert("firstvalue=4");
+alert("seconvalue=10");
+var percentage=(firstvalue/secondvalue*100);
+alert("result in percentage=");
